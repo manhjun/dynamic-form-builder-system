e6c33b55eda1245706bc6ed3f96e000f9e5efb5d
dungnm <dungnm.web@gmail.com>
2026-05-02 18:00:05 +0700

    update: alter table form

--- root/database/migrations/2026_05_02_154550_create_forms_table.php
+++ root/database/migrations/2026_05_02_154550_create_forms_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
 
             $table->string('title');
             $table->text('description')->nullable();
+            $table->unsignedInteger('order')->default(0);
 
             $table->timestamps();
         });
