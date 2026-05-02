1283424ef3e08d8b6d37890b45e638b6995ad382
dungnm <dungnm.web@gmail.com>
2026-05-02 20:20:30 +0700

    feat: add API routes

--- root/routes/api.php
+++ root/routes/api.php
@@ -1,19 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\FieldController;
+use App\Http\Controllers\Api\FormController;
+use App\Http\Controllers\Api\SubmissionController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('forms/active', [SubmissionController::class, 'activeForms']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Form Management
+Route::apiResource('forms', FormController::class);
+
+// Field Management
+Route::post('forms/{form}/fields', [FieldController::class, 'store']);
+Route::put('forms/{form}/fields/{field}', [FieldController::class, 'update']);
+Route::delete('forms/{form}/fields/{field}', [FieldController::class, 'destroy']);
+
+// Submission
+Route::post('forms/{form}/submit', [SubmissionController::class, 'submit']);
+Route::get('submissions', [SubmissionController::class, 'index']);
